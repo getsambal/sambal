31fed72aedfc4a9380d8fd724cf1248113e2710f
Joel Yek <joevo2@gmail.com>
2016-10-08 14:26:21 +0800

    added card detail screen

--- navigation/Router.js
+++ navigation/Router.js
@@ -3,9 +3,11 @@ import {
 } from '@exponent/ex-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
+import CardDetail from '../screens/CardDetail';
 import RootNavigation from './RootNavigation';
 
 export default createRouter(() => ({
   home: () => HomeScreen,
+  cardDetail: () => CardDetail,
   rootNavigation: () => RootNavigation,
 }));
---
+++ screens/CardDetail.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default class HomeScreen extends React.Component {
+  static route = {
+    navigationBar: {
+      title: 'Card Details',
+      visible: true,
+    },
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Card Details</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
