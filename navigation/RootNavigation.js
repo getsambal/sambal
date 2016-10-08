ba6c59ce4793e2451b6127e35fcea526395f18c5
Joel Yek <joevo2@gmail.com>
2016-10-08 11:09:25 +0800

    Removed all unused stuff - Remove unused screen and function - Left tab navigation

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -15,19 +15,10 @@ import {
   FontAwesome,
 } from '@exponent/vector-icons';
 
-import Alerts from '../constants/Alerts';
 import Colors from '../constants/Colors';
 import Router from '../navigation/Router';
-import registerForPushNotificationsAsync from '../api/registerForPushNotificationsAsync';
 
 export default class RootNavigation extends React.Component {
-  componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications();
-  }
-
-  componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove();
-  }
 
   render() {
     return (
@@ -38,18 +29,6 @@ export default class RootNavigation extends React.Component {
           id="home"
           renderIcon={isSelected => this._renderIcon('home', isSelected)}>
           <StackNavigation initialRoute={Router.getRoute('home')} />
-        </TabNavigationItem>
-
-        <TabNavigationItem
-          id="links"
-          renderIcon={isSelected => this._renderIcon('book', isSelected)}>
-          <StackNavigation initialRoute={Router.getRoute('links')} />
-        </TabNavigationItem>
-
-        <TabNavigationItem
-          id="settings"
-          renderIcon={isSelected => this._renderIcon('cog', isSelected)}>
-          <StackNavigation initialRoute={Router.getRoute('settings')} />
         </TabNavigationItem>
       </TabNavigation>
     );
@@ -62,29 +41,6 @@ export default class RootNavigation extends React.Component {
         size={32}
         color={isSelected ? Colors.tabIconSelected : Colors.tabIconDefault}
       />
-    );
-  }
-
-  _registerForPushNotifications() {
-    const { notification } = this.props;
-
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync();
-
-    // If we started the app from a push notification, handle it right away
-    notification && this._handleNotification(notification);
-
-    // Handle notifications that come in while the app is open
-    return DeviceEventEmitter.addListener('Exponent.notification', this._handleNotification);
-  }
-
-  _handleNotification = ({origin, data}) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${data}`,
-      Alerts.notice
     );
   }
 }
--- navigation/Router.js
+++ navigation/Router.js
@@ -3,13 +3,9 @@ import {
 } from '@exponent/ex-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 import RootNavigation from './RootNavigation';
 
 export default createRouter(() => ({
   home: () => HomeScreen,
-  links: () => LinksScreen,
-  settings: () => SettingsScreen,
   rootNavigation: () => RootNavigation,
 }));
