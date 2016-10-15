7933e4e1c2573bcb82ba3e72bd123171d807ac41
burhan3759 <bern.3759@yahoo.com>
2016-10-15 12:24:12 +0800

    added a file

--- navigation/Router.js
+++ navigation/Router.js
@@ -6,8 +6,10 @@ import HomeScreen from '../screens/HomeScreen';
 import CardDetail from '../screens/CardDetail';
 import AddFood from '../screens/AddFood';
 import RootNavigation from './RootNavigation';
+import MapComponent from '../components/mapComponent';
 
 export default createRouter(() => ({
+	map: () => MapComponent,
   home: () => HomeScreen,
   cardDetail: () => CardDetail,
   addFood: () => AddFood,
