d8a9da428198e2870f866745f8c477dea81abe4c
burhan3759 <bern.3759@yahoo.com>
2016-10-15 12:26:39 +0800

    add a file

---
+++ components/mapComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+export default class mapComponent extends Component{
+
+
+	render(){
+		return(
+			<View>
+				<Text>hello Map</Text>
+			</View>
+		)
+	}
+}
