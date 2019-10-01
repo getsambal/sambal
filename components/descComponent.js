75a644920b7cd5d590e4870968384fd4789c1fc8
ananta bastola <anantarajbastola@gmail.com>
2019-10-01 15:03:24 +0545

    Minor refactor.

--- components/descComponent.js
+++ components/descComponent.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Card, ListItem, Button } from 'react-native-elements';
 import { RegularText } from './StyledText';
 
 import Layouts from '../constants/Layout';
 const Layout = Layouts.window;
 
-let desc = "This is food description";
+let desc = 'This is food description';
 
-export default class descComponent extends Component{
-	render(){
-		return(
-			<View style={{flex:1, backgroundColor: '#FAFAFA'}}>
+export default class descComponent extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, backgroundColor: '#FAFAFA' }}>
+        <Text style={{ fontSize: 16, fontWeight: '500', margin: 5 }}>
+          Description
+        </Text>
 
-				<Text style={{fontSize: 16, fontWeight: '500', margin: 5}}>Description</Text>
-
-				<View style={{height: Layout.height/6, backgroundColor: '#fff'}}>
-					<RegularText style={{margin: 5, fontSize: 14}}>
-						{this.props.desc}
-					</RegularText>
-				</View>
-
-			</View>
-		)
-	}
+        <View
+          style={{
+            height: Layout.height / 6,
+            backgroundColor: '#fff',
+          }}
+        >
+          <RegularText style={{ margin: 5, fontSize: 14 }}>
+            {this.props.desc}
+          </RegularText>
+        </View>
+      </View>
+    );
+  }
 }
---
+++ screens/ExpandedView.js
@@ -0,0 +1,199 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  Animated,
+  StyleSheet,
+  Platform,
+  Linking,
+} from 'react-native';
+
+import Map from '../components/mapComponent';
+import Desc from '../components/descComponent';
+import Image from '../components/imageComponent';
+import Layout from '../constants/Layout';
+import Price from '../components/Cards';
+import Food from '../components/FoodDetails';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const data = {
+  data_: {
+    image:
+      'https://img.grouponcdn.com/seocms/swZZyUMNMcZ84TUGjiQLLq/malaysian-food_hero-600x390.jpg',
+    desc: 'Food desc...',
+    price: 'RM 4.50 - 6.00',
+    name: 'Nasi Lemak',
+    restaurant: 'Nasi Lemak Sedap',
+  },
+};
+
+export default class ExpandedView extends Component {
+  state = {
+    scrollY: new Animated.Value(0),
+    address: 'Aeon Big',
+    postalCode: '53300',
+    city: 'wangsa maju',
+  };
+
+  _handlePressDirections = () => {
+    let { address, postalCode, city } = this.state;
+
+    let daddr = encodeURIComponent(`${address} ${postalCode}, ${city}`);
+
+    if (Platform.OS === 'ios') {
+      Linking.openURL(`http://maps.apple.com/?daddr=${daddr}`);
+    } else {
+      Linking.openURL(`http://maps.google.com/?daddr=${daddr}`);
+    }
+  };
+
+  render() {
+    let { scrollY } = this.state;
+    return (
+      <View style={{ flex: 1, backgroundColor: '#FAFAFA' }}>
+        <View style={{ flex: 1, marginTop: -70 }}>
+          {this._renderHeroHeader()}
+
+          <Animated.ScrollView
+            scrollEventThrottle={16}
+            style={StyleSheet.absoluteFill}
+            onScroll={Animated.event(
+              [
+                {
+                  nativeEvent: {
+                    contentOffset: { y: scrollY },
+                  },
+                },
+              ],
+              { useNativeDriver: true }
+            )}
+          >
+            <View
+              style={{
+                width: Layout.window.width,
+                height: 370,
+                backgroundColor: 'transparent',
+              }}
+            />
+
+            <View
+              style={{
+                paddingBottom: 20,
+                backgroundColor: '#FAFAFA',
+                minHeight: Layout.window.height - 370,
+              }}
+            >
+              <Food details={data.data_} />
+              <Map onPress={this._handlePressDirections} />
+              <Price price={data.data_.price} />
+              <Desc desc={data.data_.desc} />
+            </View>
+          </Animated.ScrollView>
+        </View>
+      </View>
+    );
+  }
+
+  _renderHeroHeader() {
+    let { scrollY } = this.state;
+
+    let logoScale = scrollY.interpolate({
+      inputRange: [-150, 0, 150],
+      outputRange: [1.5, 1, 1],
+    });
+
+    let logoTranslateY = scrollY.interpolate({
+      inputRange: [-150, 0, 150],
+      outputRange: [40, 0, -40],
+    });
+
+    let logoOpacity = scrollY.interpolate({
+      inputRange: [-150, 0, 200, 400],
+      outputRange: [1, 1, 0.2, 0.2],
+    });
+
+    let heroBackgroundTranslateY = scrollY.interpolate({
+      inputRange: [-1, 0, 200, 201],
+      outputRange: [0, 0, -400, -400],
+    });
+
+    let gradientTranslateY = scrollY.interpolate({
+      inputRange: [-1, 0, 1],
+      outputRange: [1, 0, -1],
+    });
+
+    return (
+      <View>
+        <Animated.View
+          style={[
+            styles.AnimatedView,
+            {
+              transform: [{ translateY: heroBackgroundTranslateY }],
+            },
+          ]}
+        />
+
+        <View style={styles.ImageContainerView}>
+          <Animated.Image
+            source={{ uri: data.data_.image }}
+            style={[
+              styles.AnimatedImage,
+              {
+                transform: [
+                  { scale: logoScale },
+                  { translateY: logoTranslateY },
+                ],
+                opacity: logoOpacity,
+              },
+            ]}
+            resizeMode="cover"
+          />
+          <Animated.View
+            style={[
+              styles.AnimatedViewGradient,
+              { transform: [{ translateY: gradientTranslateY }] },
+            ]}
+          >
+            <LinearGradient
+              colors={['transparent', 'rgba(0,0,0,0.07)']}
+              style={styles.LinearGradient}
+            />
+          </Animated.View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  AnimatedView: {
+    height: 370 + 250,
+    backgroundColor: '#FAFAFA',
+  },
+  ImageContainerView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 370,
+    paddingTop: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  AnimatedImage: {
+    width: Layout.window.width,
+    height: Layout.window.height / 2,
+    marginTop: 80,
+  },
+  AnimatedViewGradient: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  LinearGradient: {
+    width: Layout.window.width,
+    height: 30,
+  },
+});
